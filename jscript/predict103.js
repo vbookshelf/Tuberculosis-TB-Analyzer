13625102b015a069b61c2a2f3f9e445439992cda
vbookshelf <44088528+vbookshelf@users.noreply.github.com>
2020-03-23 19:14:16 +0700

    Added batch processing capability. Updated the UI.
    The app is now mobile optimized. It's also able to process batches of images instead of just one image at a time.

--- jscript/predict103.js
+++ jscript/predict103.js
@@ -45,8 +45,8 @@ $("#image-selector").change(function () {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('http://tb.test.woza.work/model_tb_1/model.json');
-	$("#selected-image").attr("src", "http://tb.test.woza.work/assets/tb.jpg")
+	model = await tf.loadModel('http://concept.test.woza.work/model_tb_1/model.json');
+	$("#selected-image").attr("src", "http://concept.test.woza.work/assets/tb.jpg")
 	
 	
 	
